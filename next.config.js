b7f4ed036214bc9fbe4d013ca80c474191c54460
williamRR <williamRR@users.noreply.github.com>
2026-01-06 14:58:40 -0300

    Fix Next.js config: remove invalid allowedDevOrigins

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    allowedDevOrigins: [
-      // Permite acceso desde cualquier IP de red local
-      /^http:\/\/192\.168\.\d+\.\d+(:\d+)?$/,
-      /^http:\/\/10\.\d+\.\d+\.\d+(:\d+)?$/,
-      /^http:\/\/172\.(1[6-9]|2\d|3[01])\.\d+\.\d+(:\d+)?$/,
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
